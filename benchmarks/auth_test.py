584b05a4fc6a935bb6ad42ee9132357db77605b9
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-07-21 15:06:07 +0700

    Change test function in auth to do register than login

--- benchmarks/auth_test.py
+++ benchmarks/auth_test.py
@@ -2,21 +2,18 @@ from locust import HttpUser, task, between
 
 from test_data import random_User
 
-user = random_User()
-
 
 class AuthTest(HttpUser):
     host = 'http://localhost:80'
     wait_time = between(2, 4)
+    user = random_User()
     token = ""
 
-    def on_start(self):
-        self.client.post('/auth/register', json=user)
-
     @task
-    def login(self):
-        response = self.client.post('/auth/login', json=user)
+    def register_and_login(self):
+        self.client.post('/auth/register', json=self.user)
+        response = self.client.post('/auth/login', json=self.user)
         self.token = response.json()['token']
 
     def on_stop(self):
-        self.client.delete('/auth/remove', headers={'Authorization', self.token}, json=user)
+        self.client.delete('/auth/remove', headers={'Authorization', self.token}, json=self.user)
